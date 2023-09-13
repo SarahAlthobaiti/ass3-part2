b257bcec1c48847c7c31edee2383604847d1a13f
SarahAlthobaiti <120081509+SarahAlthobaiti@users.noreply.github.com>
2023-09-13 22:01:49 +0300

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,26 @@
+//Student Name : Sarah Mohammed Althobiti , Student ID : 444004881
+public interface Stack<E> {
+    
+    int size();
+
+
+
+    boolean isEmpty();
+
+
+
+
+
+    void push(E e);
+
+
+
+
+    E top();
+
+
+
+
+    E pop();
+
+}
